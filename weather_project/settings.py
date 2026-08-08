b3bf7942f6ffee822938c39d6d42230225954ff8
Mohib Qureshi <umohib7800@gmail.com>
2026-08-08 15:35:25 +0500

    Prepare Django app for deployment

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -7,12 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-SECRET_KEY = 'YOUR_SECRET_KEY_HERE'
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 
